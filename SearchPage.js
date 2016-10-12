094d28029dac204af7b042b594b2978833493799
Nick Martinez <23nicholasmartinez@gmail.com>
2016-10-12 16:34:35 -0700

    Set API request and response handling

--- SearchPage.js
+++ SearchPage.js
@@ -85,7 +85,8 @@ class SearchPage extends Component {
     super(props);
     this.state = {
       searchString: 'london',
-      isLoading: false
+      isLoading: false,
+      message: ''
     };
   }
   // Event handler method to handle text changes by the user.
@@ -95,7 +96,23 @@ class SearchPage extends Component {
   _executeQuery(query) {
     console.log(query);
     this.setState({isLoading: true });
+    fetch(query)
+      .then(response => response.json())
+      .then(json => this._handleResponse(json.response))
+      .catch(error =>
+        this.setState({
+          isLoading: false,
+          message: 'Something bad happened' + error
+        }));
   }
+  _handleResponse(response) {
+  this.setState({ isLoading: false, message: '' });
+  if (response.application_response_code.substr(0, 1) === '1') {
+    console.log('Properties found: ' + response.listings.length);
+  } else {
+    this.setState({ message: 'Location not recognized; please try again.' });
+  }
+}
   onSearchPressed() {
     var query = urlForQueryAndPage('place_name', this.state.searchString, 1);
     this._executeQuery(query);
@@ -130,6 +147,7 @@ class SearchPage extends Component {
         </TouchableHighlight>
         <Image source={require('./Resources/house.png')} style={styles.image}/>
         {spinner}
+        <Text style={styles.description}>{this.state.message}</Text>
       </View>
     );
   }
