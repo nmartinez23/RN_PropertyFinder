117e47819ddeb849a899edc6d4cb879b75a14c70
Nick Martinez <23nicholasmartinez@gmail.com>
2016-10-13 09:46:54 -0700

    Created property view details and styles

--- SearchResults.js
+++ SearchResults.js
@@ -10,6 +10,8 @@ import {
   Text
 } from 'react-native';
 
+var PropertyView = require('./PropertyView');
+
 var styles = StyleSheet.create({
   thumb: {
     width: 80,
@@ -72,6 +74,12 @@ class SearchResults extends Component {
 
   rowPressed(listerURL) {
     var property = this.props.listings.filter(prop => prop.lister_url === listerURL)[0];
+
+    this.props.navigator.push({
+      title: "Property",
+      component: PropertyView,
+      passProps: {property: property}
+    });
   }
 
   render() {
